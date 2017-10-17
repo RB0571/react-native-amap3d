db46988bd9c8d889194fe14b65f9f24a846a2ca7
shuke <shukelin@126.com>
2017-10-17 15:28:11 +0800

    fix listener

--- components/MapLocation.js
+++ components/MapLocation.js
@@ -9,21 +9,21 @@ var {
 
 const AMapLocation = NativeModules.AMapLocation;
 const onLocationUpdatingEvent = 'onLocationUpdatingEvent';
-
+let listener
 module.exports = {
     startUpdatingLocation: (option, callback) => {
         const event = onLocationUpdatingEvent + Math.random().toString(36).substr(2)
         const handler = (body) => {
             callback && callback(body)
-            listener && DeviceEventEmitter.removeListener(listener)
         }
-        const listener = DeviceEventEmitter.addListener(
+        listener = DeviceEventEmitter.addListener(
             event,
             handler
         );
         AMapLocation.startUpdatingLocation(option, event)
     },
     stopUpdatingLocatoin: () => {
+        listener && DeviceEventEmitter.removeListener(listener)
         AMapLocation.stopUpdatingLocatoin()
     }
 };
